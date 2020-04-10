8f9a82ffb478f63954c7107764a96c1e44cb0589
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-10 17:37:24 +0300

    create main file with documentation

---
+++ section1-print_array/main.c
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (10/4/2020)
+Purpose: This file initiate array in size 10, that contain pointers to
+		 addresses of 10 int variables, randomize the int variables,
+		 print the index in array, the address, and the value	
+\********************************************************/
