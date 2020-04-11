49d2766b81142a977de56d898049098dd607a957
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-11 12:19:38 +0300

    add main file with documentation

---
+++ section4-sort_values_by_swap_values/main.c
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (11/4/2020)
+Purpose: This file initiate array in size 10, that contain pointers to
+		 addresses of 10 int variables, randomize the int variables,
+		 print the index in array, the address, and the value,
+		 and sort the values of pointer array by swap values
+\********************************************************/
